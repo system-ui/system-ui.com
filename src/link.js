7a60794ad08ec7bed56fda1c47cfa0b26ef95cb6
Brent Jackson <jxnblk@gmail.com>
2019-03-23 13:34:51 -0400

    Add theme specification page

---
+++ src/link.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import isAbsoluteURL from 'is-absolute-url'
+
+export default ({ href = '', ...props }) => isAbsoluteURL(href)
+  ? <a href={href} {...props} />
+  : <Link to={href} {...props} />
